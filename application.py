e6162e1fbec467c2329e227a29c98838a0ea0d2f
ARJUN198SINGH <charjunsingh198@gmail.com>
2023-05-05 17:31:36 +0530

    first commit

--- application.py
+++ application.py
@@ -9,8 +9,8 @@ application = Flask(__name__)
 app=application
 
 
-model=pickle.load(open('models/model of drugs.pkl','rb'))
-standard_scaler=pickle.load(open('models/standard drugs.pkl','rb'))
+model=pickle.load(open('models/model for carped.pkl','rb'))
+standard_scaler=pickle.load(open('models/scaler for carped.pkl','rb'))
 # pca=pickle.load(open('models/pcapokemon.pkl','rb'))
 
 
@@ -21,39 +21,58 @@ def index():
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='POST':
-        Age=float(request.form.get('Age'))
-        Na_to_K = float(request.form.get('Na_to_K'))
-        Sex_F = float(request.form.get('Sex_F'))
-        Sex_M = float(request.form.get('Sex_M'))
-        BP_HIGH = float(request.form.get('BP_HIGH'))
-        BP_LOW= float(request.form.get('BP_LOW'))
-
-        BP_NORMAL = float(request.form.get('BP_NORMAL'))
-        Cholesterol_HIGH= float(request.form.get('Cholesterol_HIGH'))
-        Cholesterol_NORMAL = float(request.form.get('Cholesterol_NORMAL'))
-        # smoker_yes = float(request.form.get('smoker_yes'))
-        # smoker_yes = float(request.form.get('smoker_yes'))
-        # smoker_yes = float(request.form.get('smoker_yes'))
-        # Speed	 = float(request.form.get('Speed'))
+        symboling=float(request.form.get('symboling'))
+        carbody = float(request.form.get('carbody'))
+        wheelbase = float(request.form.get('wheelbase'))
+        carlength = float(request.form.get('carlength'))
+        carwidth = float(request.form.get('carwidth'))
+        carheight= float(request.form.get('carheight'))
+        curbweight = float(request.form.get('curbweight'))
         
-        # Generation	 = float(request.form.get('Generation'))
+        enginetype = float(request.form.get('enginetype'))
+        cylindernumber = float(request.form.get('cylindernumber'))
+        enginesize = float(request.form.get('enginesize'))
+        fuelsystem	 = float(request.form.get('fuelsystem'))
+        boreratio = float(request.form.get('boreratio'))
+        stroke = float(request.form.get('stroke'))
+        compressionratio = float(request.form.get('compressionratio'))
+        horsepower = float(request.form.get('horsepower'))
+        peakrpm = float(request.form.get('peakrpm'))
+        citympg = float(request.form.get('citympg'))
+        highwaympg = float(request.form.get('highwaympg'))
+        fueltype_diesel = float(request.form.get('fueltype_diesel'))
+        fueltype_gas = float(request.form.get('fueltype_gas'))
+        aspiration_std = float(request.form.get('aspiration_std'))
+
+        aspiration_turbo = float(request.form.get('aspiration_turbo'))
+        doornumber_four = float(request.form.get('doornumber_four'))
+        doornumber_two = float(request.form.get('doornumber_two'))
+        drivewheel_4wd = float(request.form.get('drivewheel_4wd'))
+        drivewheel_fwd = float(request.form.get('drivewheel_fwd'))
+        drivewheel_rwd = float(request.form.get('drivewheel_rwd'))
+        enginelocation_front = float(request.form.get('enginelocation_front'))
+        enginelocation_rear = float(request.form.get('enginelocation_rear'))
+
+
+        
     
-        new_data_scaled=standard_scaler.transform([ [Age,Na_to_K,Sex_F,Sex_M,BP_HIGH,BP_LOW,BP_NORMAL,Cholesterol_HIGH,Cholesterol_NORMAL]])
+        new_data_scaled=standard_scaler.transform([[symboling, carbody, wheelbase, carlength, carwidth,carheight, curbweight, enginetype, cylindernumber, enginesize,fuelsystem, boreratio, stroke, compressionratio, horsepower,peakrpm, citympg, highwaympg, fueltype_diesel, fueltype_gas,aspiration_std, aspiration_turbo, doornumber_four,
+        doornumber_two, drivewheel_4wd, drivewheel_fwd, drivewheel_rwd,enginelocation_front, enginelocation_rear]])
         # new_data_pca=pca.transform(new_data_scaled)
         result=model.predict(new_data_scaled)
-        if(result[0]==0):
-            result='DrugY'
-        elif(result[0]==1):
-            result='DrugC'
-        elif(result[0]==2):
-            result='DrugX'
-        elif(result[0]==3):
-            result='DrugA'
-        else:
-            result='DrugB'
-        return render_template('drugs.html',result=result)
+        # if(result[0]==0):
+        #     result='DrugY'
+        # elif(result[0]==1):
+        #     result='DrugC'
+        # elif(result[0]==2):
+        #     result='DrugX'
+        # elif(result[0]==3):
+        #     result='DrugA'
+        # else:
+        #     result='DrugB'
+        return render_template('car pred.html',result=result)
     else:  
-        return render_template('drugs.html')
+        return render_template('car pred.html')
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
