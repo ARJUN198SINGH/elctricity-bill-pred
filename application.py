9ed8f5e5c023ba6c16f3b1b0d4ebc98ce6ce91d5
ARJUN198SINGH <CHARJUNSINGH198@GMAIL.COM>
2023-05-16 11:32:39 +0530

    first commit

--- application.py
+++ application.py
@@ -9,8 +9,8 @@ application = Flask(__name__)
 app=application
 
 
-model=pickle.load(open('models/model for carped.pkl','rb'))
-standard_scaler=pickle.load(open('models/scaler for carped.pkl','rb'))
+model=pickle.load(open('models/model of elicticity bill.pkl','rb'))
+standard_scaler=pickle.load(open('models/scaling of elicticity bill.pkl','rb'))
 # pca=pickle.load(open('models/pcapokemon.pkl','rb'))
 
 
@@ -21,44 +21,40 @@ def index():
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='POST':
-        symboling=float(request.form.get('symboling'))
-        carbody = float(request.form.get('carbody'))
-        wheelbase = float(request.form.get('wheelbase'))
-        carlength = float(request.form.get('carlength'))
-        carwidth = float(request.form.get('carwidth'))
-        carheight= float(request.form.get('carheight'))
-        curbweight = float(request.form.get('curbweight'))
-        
-        enginetype = float(request.form.get('enginetype'))
-        cylindernumber = float(request.form.get('cylindernumber'))
-        enginesize = float(request.form.get('enginesize'))
-        fuelsystem	 = float(request.form.get('fuelsystem'))
-        boreratio = float(request.form.get('boreratio'))
-        stroke = float(request.form.get('stroke'))
-        compressionratio = float(request.form.get('compressionratio'))
-        horsepower = float(request.form.get('horsepower'))
-        peakrpm = float(request.form.get('peakrpm'))
-        citympg = float(request.form.get('citympg'))
-        highwaympg = float(request.form.get('highwaympg'))
-        fueltype_diesel = float(request.form.get('fueltype_diesel'))
-        fueltype_gas = float(request.form.get('fueltype_gas'))
-        aspiration_std = float(request.form.get('aspiration_std'))
+        num_rooms=float(request.form.get('num_rooms'))
+        num_people = float(request.form.get('num_people'))
+        housearea = float(request.form.get('housearea'))
+        is_ac = float(request.form.get('is_ac'))
+        is_tv = float(request.form.get('is_tv'))
+        is_flat= float(request.form.get('is_flat'))
+        ave_monthly_income = float(request.form.get('ave_monthly_income'))
+        num_children= float(request.form.get('num_children'))
+        is_urban = float(request.form.get('is_urban'))
+       
 
-        aspiration_turbo = float(request.form.get('aspiration_turbo'))
-        doornumber_four = float(request.form.get('doornumber_four'))
-        doornumber_two = float(request.form.get('doornumber_two'))
-        drivewheel_4wd = float(request.form.get('drivewheel_4wd'))
-        drivewheel_fwd = float(request.form.get('drivewheel_fwd'))
-        drivewheel_rwd = float(request.form.get('drivewheel_rwd'))
-        enginelocation_front = float(request.form.get('enginelocation_front'))
-        enginelocation_rear = float(request.form.get('enginelocation_rear'))
+        # stroke = float(request.form.get('stroke'))
+        # compressionratio = float(request.form.get('compressionratio'))
+        # horsepower = float(request.form.get('horsepower'))
+        # peakrpm = float(request.form.get('peakrpm'))
+        # citympg = float(request.form.get('citympg'))
+        # highwaympg = float(request.form.get('highwaympg'))
+        # fueltype_diesel = float(request.form.get('fueltype_diesel'))
+        # fueltype_gas = float(request.form.get('fueltype_gas'))
+        # aspiration_std = float(request.form.get('aspiration_std'))
+
+        # aspiration_turbo = float(request.form.get('aspiration_turbo'))
+        # doornumber_four = float(request.form.get('doornumber_four'))
+        # doornumber_two = float(request.form.get('doornumber_two'))
+        # drivewheel_4wd = float(request.form.get('drivewheel_4wd'))
+        # drivewheel_fwd = float(request.form.get('drivewheel_fwd'))
+        # drivewheel_rwd = float(request.form.get('drivewheel_rwd'))
+        # enginelocation_front = float(request.form.get('enginelocation_front'))
+        # enginelocation_rear = float(request.form.get('enginelocation_rear'))
 
 
-        
-    
-        new_data_scaled=standard_scaler.transform([[symboling, carbody, wheelbase, carlength, carwidth,carheight, curbweight, enginetype, cylindernumber, enginesize,fuelsystem, boreratio, stroke, compressionratio, horsepower,peakrpm, citympg, highwaympg, fueltype_diesel, fueltype_gas,aspiration_std, aspiration_turbo, doornumber_four,
-        doornumber_two, drivewheel_4wd, drivewheel_fwd, drivewheel_rwd,enginelocation_front, enginelocation_rear]])
-        # new_data_pca=pca.transform(new_data_scaled)
+        q=np.array([[num_rooms, num_people, housearea, is_ac, is_tv, is_flat,ave_monthly_income, num_children, is_urban]])
+        new_data_scaled=standard_scaler.transform(q)
+      
         result=model.predict(new_data_scaled)
         # if(result[0]==0):
         #     result='DrugY'
@@ -70,9 +66,9 @@ def predict_datapoint():
         #     result='DrugA'
         # else:
         #     result='DrugB'
-        return render_template('car pred.html',result=result)
+        return render_template('Household energy bill data.html',result=np.round(result,2))
     else:  
-        return render_template('car pred.html')
+        return render_template('Household energy bill data.html')
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
